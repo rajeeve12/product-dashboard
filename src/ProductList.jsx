e39f37acd078f70680d5151f41e9d9d743c18d64
rajeeve12 <rajeevevishen21@gmal.com>
2024-11-17 12:34:49 -0500

    Create ProductList component to render each ProductItem

---
+++ src/ProductList.jsx
@@ -0,0 +1,11 @@
+
+function ProductList({ products }) {
+    if (!products || products.length === 0) {
+        return (
+            No); products; available;
+    }
+}
+
+
+    export default ProductList;
+  
