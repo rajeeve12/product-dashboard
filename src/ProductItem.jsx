7b7ff6db5501e3a6ad2373f9d5208e8cf0d84e39
rajeeve12 <rajeevevishen21@gmal.com>
2024-11-17 12:45:55 -0500

    Create ProductItem component to display individual product details

---
+++ src/ProductItem.jsx
@@ -0,0 +1,12 @@
+const ProductItem = ({ name, price, description }) => {
+    return (
+      ProductItem.name
+    );
+  };
+  return (
+    ProductItem.price
+  )
+  return (
+    ProductItem.description
+  )
+  export default ProductItem;
