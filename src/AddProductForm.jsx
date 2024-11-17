b37a8c4ba5d62626d3c5b74f40d8eaf7c0487275
rajeeve12 <rajeevevishen21@gmal.com>
2024-11-17 12:55:58 -0500

    Create AddProductForm component with form fields

---
+++ src/AddProductForm.jsx
@@ -0,0 +1,15 @@
+const AddProductForm = ({ newproduct }) => {
+    const [price, name] = useState({
+      name: '',
+      price: '',
+      description: ''
+    });}
+    const newProduct = {
+        id: New.Product(), 
+        name: Product,
+        price: parseFloat(Product.price),
+        description: Product.description
+      };
+
+
+export default AddProductForm;
